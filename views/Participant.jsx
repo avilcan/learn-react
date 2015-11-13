e04bfd2b0bc73accf2d58be4154543c53940fc6e
alexandravilcan <alexandravilcan@yahoo.com>
2015-11-13 19:44:48 +0200

    update

--- views/Participant.jsx
+++ views/Participant.jsx
@@ -1,6 +1,35 @@
+var classNames = require("classnames");
 module.exports = React.createClass({
+	getInitialState:function(){
+		return {
+			expanded:true
+		}
+	},
+	rowClicked:function(){
+		this.setState({
+			expanded:!this.state.expanded
+		});
+	},
+	getExpandedRow:function(){
+		return (<tr onClick={this.rowClicked}>
+					<th></th>
+					<td></td>
+					<td>{this.props.participantData.name}</td>
+					<td>{this.props.participantData.age}</td>
+					<td>{this.props.participantData.gender}</td>
+					<td></td>
+				</tr>);
+	},
+	getCollapsedRow:function(){
+		return (<tr></tr>);
+	},
 	render:function(){
-		return (<tr>
+		var expandCollapseClasses = classNames({
+			"glyphicon":true,
+			"glyphicon-minus":this.state.expanded,
+			"glyphicon-plus":!this.state.expanded
+		})
+		return (<tr onClick={this.rowClicked}>
 					<th></th>
 					<td></td>
 					<td>{this.props.participantData.name}</td>
