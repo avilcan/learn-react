7c4adb1c1cdef1ca53d7a8032bf87d54e9952351
alexandravilcan <alexandravilcan@yahoo.com>
2015-12-03 22:10:18 +0200

    pure render mixin dependency and update participant method

--- actions/Actions.js
+++ actions/Actions.js
@@ -2,7 +2,8 @@ var Reflux = require("reflux");
 window.Actions = {
 	"addParticipant":Reflux.createAction(),
 	"like":Reflux.createAction(),
-	"unlike":Reflux.createAction()
+	"unlike":Reflux.createAction(),
+	"updateParticipant":Reflux.createAction()
 }
 
 module.exports = window.Actions;
--- stores/Participants.jsx
+++ stores/Participants.jsx
@@ -1,15 +1,23 @@
 var participants = require("../data/Participants.json");
 var Reflux = require("reflux");
 var Actions = require("../actions/Actions");
-var Immutable = require("immutable");
+var _ = require("underscore");
 module.exports = Reflux.createStore({
 	init:function(){
 		this._participants = null;
 		this.listenTo(Actions.addParticipant,this.addParticipant);
+		this.listenTo(Actions.updateParticipant,this.updateParticipant);
 	},
 	addParticipant:function(data){
 		this._participants.push(data);
 		this.trigger();
+	},
+	updateParticipant:function(id,data){
+		var participant = _.find(this._participants,p=>p._id === id);
+		if (participant) {
+			_.extend(participant,data);
+			this.trigger();
+		}
 	},
 	getParticipants:function(){
 		if (!this._participants){
